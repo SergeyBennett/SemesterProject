fa9abf73d5e7b34da446d75e1778e2ddb22fba34
SergeyBennett <Sergeybennett@gmail.com>
2016-06-23 19:07:47 +0300

    Update1

---
+++ Project/www/js/Books.js
@@ -0,0 +1,23 @@
+var all_books = [
+    {
+        id:1,
+        icon:'assets/images/pizza_7.jpg',
+        title: "Book1",
+        genre:'Philosophy',
+        review: 'I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. I hate philosophy. ',
+        is_new:false,
+        is_popular:false
+    },
+    {
+        id:1,
+        icon:'assets/images/pizza_7.jpg',
+        title: "Book2",
+        genre:'Science fiction',
+        review: 'But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! But I like science fiction! ',
+        is_new:true,
+        is_popular:true
+
+    }
+];
+
+module.exports = all_books;
